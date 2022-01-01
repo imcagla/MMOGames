ee6d29946edf4ffb119efc1aa066c17a6f1845f3
Cagla <caglaeyubog@gmail.com>
2022-01-02 01:15:08 +0300

    games detail button changed/ style updated

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from 'react'
-import {Link} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 import "../styles/games.css"
 
 function Games() {
     const [data, setData] = useState([])
     const [loading, setLoading] = useState(true);
+    const navigate = useNavigate()
 
     useEffect(() => {
         fetch("https://mmo-games.p.rapidapi.com/games", {
@@ -28,27 +29,27 @@ function Games() {
     return (
         <div className="container">
             <div className='row my-4'>
-            <table className="table table-dark">
+            <table className="table table-dark table-hover table-bordered border-info align-middle">
             <thead className='text-info'>
                   <tr>
                     <th scope="col"></th>
                     <th scope="col">Thumbnail</th>
                     <th scope="col">Title</th>
                     <th scope="col">Description</th>
-                    <th scope="col"></th>
                   </tr>
                 </thead>
                 <tbody>
                 {
-                    data.map((item, index) =>  <tr key={index} className='border border-info align-middle'>
+                    data.map((item, index) =>  <tr key={index} >
                                         <th scope="row">{index+1}</th>
                                         <td> <img src={item.thumbnail} alt={item.title} className='img-fluid' width="200" /></td>
-                                        <td>{item.title} <p className='text-muted'>{item.genre}</p></td>
+                                        <td>{item.title} <p className='text-muted'>{item.genre}</p> </td>
                                         
-                                        <td>{item.short_description}</td>
-                                        <td><Link to={`/games/${item.id}`} className="btn btn-outline-info w-100">
-                                                See Detail
-                                            </Link></td>
+                                        <td> 
+                                            <span className="body-overflow"> {item.short_description}</span>
+                                            <span onClick={() => navigate(`${index+1}`)} ><i className="fas fa-angle-double-right cursor-pointer float-end text-info"></i></span>
+                                        </td>
+
                                     </tr>
                     )
                 }
