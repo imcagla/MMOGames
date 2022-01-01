948d99f4cae9b21858516386ac9d563c447028eb
Cagla <caglaeyubog@gmail.com>
2022-01-02 01:28:48 +0300

    carousel added to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,28 @@ function Home() {
     
 
     return (
-        <div className='text-center' >
-            <img className='pt-5 py-3 img-fluid' src="https://www.mmobomb.com/g/45/thumbnail.jpg" width="400" alt="" />
+        <div className='text-center my-5' >
+            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://www.mmobomb.com/g/1/thumbnail.jpg" width="500" alt="..."/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.mmobomb.com/g/7/thumbnail.jpg" width="500" alt="..."/>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://www.mmobomb.com/g/77/thumbnail.jpg" width="500" alt="..."/>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
             <GameSearch />
         </div>
     )
