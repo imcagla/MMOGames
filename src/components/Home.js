996af378bf86cbcc8b10e9b250d7457142c7773e
Cagla <caglaeyubog@gmail.com>
2022-01-02 01:46:08 +0300

    navbar responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,25 +7,25 @@ function Home() {
 
     return (
         <div className='text-center my-5' >
-            <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
+            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
                         <img src="https://www.mmobomb.com/g/1/thumbnail.jpg" width="500" alt="..."/>
                     </div>
-                    <div class="carousel-item">
+                    <div className="carousel-item">
                         <img src="https://www.mmobomb.com/g/7/thumbnail.jpg" width="500" alt="..."/>
                     </div>
-                    <div class="carousel-item">
+                    <div className="carousel-item">
                         <img src="https://www.mmobomb.com/g/77/thumbnail.jpg" width="500" alt="..."/>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
                 </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
                 </button>
                 </div>
             <GameSearch />
--- src/components/base/Navbar.js
+++ src/components/base/Navbar.js
@@ -5,14 +5,18 @@ import routes from '../../routes'
 function Navbar() {
     return (
         <div>
-            <div className="container">
-                <header className="d-flex justify-content-center py-3">
-                <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none">
-                    <i className="fas fa-gamepad fa-3x mx-2"></i>
-                    <span className="fs-4">MMO Games</span>
-                </Link>
-                <ul className="nav nav-pills">
-                {
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container">
+                    <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-light text-decoration-none">
+                        <i className="fas fa-gamepad fa-3x mx-2"></i>
+                        <span className="fs-4">MMO Games</span>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav ms-auto">
+                    {
                                 routes.filter(item => item.isNav).map((item,index) => 
                                         <li key={index} className="text-decoration-none nav-item">
                                             <Link  to={item.pathname} className="nav-link text-light" href="#">
@@ -20,9 +24,10 @@ function Navbar() {
                                             </Link>
                                         </li>)
                             }
-                </ul>
-                </header>
-            </div>
+                    </div>
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
